aaa63e76430de99a7917a6b500631e1d4cbc2e86
Borja <bmartinc6@gmail.com>
2020-09-30 20:55:06 +0200

    3. Collections

---
+++ simple-todos/imports/ui/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div className="container">
+    <header>
+      <h1>Todo List</h1>
+    </header>
+    <ul>
+       <Task v-for="task in tasks" 
+       v-bind:key="task._id" 
+       v-bind:task="task" />
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import Vue from "vue";
+import Task from "./Task.vue";
+import { Tasks } from "../api/tasks.js"
+
+export default {
+  components: {
+    Task
+  },
+
+  data() {
+    return {};
+  },
+    methods: {},
+    meteor: {
+    tasks() {
+        console.log( Tasks.find({}).fetch());
+        return Tasks.find({}).fetch();
+    }
+  }
+};
+
+</script>
---
+++ simple-todos/imports/ui/Task.vue
@@ -0,0 +1,26 @@
+
+
+<template>
+
+  <li>{{ this.task.text }}</li>
+
+</template>
+
+ 
+
+<script>
+
+export default {
+
+  props: ["task"],
+
+  data() {
+
+    return {};
+
+  }
+
+};
+
+</script>
+
